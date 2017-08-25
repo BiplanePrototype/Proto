6555bc38b18410d5aad5a5eeb1b8fffbb9385223
Rernokk <ssmith37@uccs.edu>
2017-08-25 15:41:51 -0600

    Foundation for Plane Details

---
+++ Biplane Inferno/Assets/Smith/Plane_Details.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum WeaponType { SINGLE, BROAD, MISSILE };
+
+public class Plane_Details : MonoBehaviour {
+    int Health = 100, MaxHealth = 100;
+    int Score = 0;
+    float DistanceTraveled = 0;
+
+    [SerializeField]
+    int AmmoRemaining = 0;
+
+    [SerializeField]
+    bool CanShoot = true;
+
+    [SerializeField]
+    float MissileCD = 3, NormalCD = .5f, BroadCD = 1f;
+
+    [SerializeField]
+    WeaponType myCurrentWeapon = WeaponType.SINGLE;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space) && CanShoot)
+        {
+            switch (myCurrentWeapon) {
+                case (WeaponType.MISSILE):
+                    if (AmmoRemaining > 0)
+                    {
+                        StartCoroutine(ShootMissile());
+                    } else
+                    {
+                        myCurrentWeapon = WeaponType.SINGLE;
+                        StartCoroutine(ShootNormal());
+                    }
+                    break;
+                case (WeaponType.BROAD):
+                    if (AmmoRemaining > 0)
+                    {
+                        StartCoroutine(ShootBroad());
+                    }
+                    else
+                    {
+                        myCurrentWeapon = WeaponType.SINGLE;
+                        StartCoroutine(ShootNormal());
+                    }
+                    break;
+                default:
+                    StartCoroutine(ShootNormal());
+                    break;
+            }
+        }
+    }
+
+    IEnumerator ShootMissile()
+    {
+        CanShoot = false;
+        AmmoRemaining--;
+        yield return new WaitForSeconds(MissileCD);
+        CanShoot = true;
+    }
+
+    IEnumerator ShootBroad()
+    {
+        CanShoot = false;
+        AmmoRemaining--;
+        yield return new WaitForSeconds(BroadCD);
+        CanShoot = true;
+    }
+
+    IEnumerator ShootNormal()
+    {
+        CanShoot = false;
+        yield return new WaitForSeconds(NormalCD);
+        CanShoot = true;
+    }
+}
