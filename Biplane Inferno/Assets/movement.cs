3c78b56fc46778d3645fc4a7d753ef4eec8380bb
jdihle <jakx76@gmail.com>
2017-08-25 22:26:10 -0600

    movement script

---
+++ Biplane Inferno/Assets/movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        if (Input.GetKey("A")||Input.GetKey(KeyCode.LeftArrow))//movement left
+        {
+            rb.MovePosition(new Vector2(rb.position.x + 1, rb.position.y));
+        }
+        if (Input.GetKey("D") || Input.GetKey(KeyCode.RightArrow))//movement right
+        {
+            //transform.Translate(-Vector3.right * 100000 * Time.deltaTime);
+            transform.position += transform.right * 1000000;
+        }
+    }
+    
+}
