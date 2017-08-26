0175ec7dfbfedc3697208bae809c79ce11abdd51
Kevin Moore <zyronn.sorrow@gmail.com>
2017-08-25 19:57:39 -0600

    Added first version of enemy logic and improved behavior of collisions

---
+++ Biplane Inferno/Assets/Main/Scripts/EnemyAI.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour {
+
+    /* SERIALIZED FIELDS */
+    [SerializeField]
+    private float speed; // speed at which enemy flies
+
+    [SerializeField]
+    private float fireDelay; // delay between enemy shots, will be randomized
+
+    [SerializeField]
+    private GameObject projectile; // the projectile object to fire
+
+    [SerializeField]
+    private float projectileSpeed; // the speed at which projectiles fly
+
+    /* PRIVATE VARIABLES */
+    private Rigidbody2D rigidBody; // rigidbody of the plane
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody2D>();
+        rigidBody.velocity = new Vector2(0, -speed);
+        StartCoroutine(Fire());
+	}
+
+    // shooting function
+    private void Shoot() {
+        Vector3 position = gameObject.transform.position;
+        position.y -= gameObject.GetComponent<Renderer>().bounds.size.y;
+        GameObject spawnedProjectile = Instantiate(projectile, position, Quaternion.identity);
+        spawnedProjectile.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -projectileSpeed);
+    }
+
+    // fire coroutine
+    IEnumerator Fire() {
+        while (true) {
+            Shoot();
+            yield return new WaitForSeconds(Random.Range(fireDelay - 1f, fireDelay + 1f));
+        }
+    }
+}
---
+++ Biplane Inferno/Assets/Main/Scripts/EnemySpawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    /* SERIALIZED FIELDS */
+
+    [SerializeField]
+    private int spawnDelay; // length of time between spawns in seconds (will be randomized)
+
+    [SerializeField]
+    private GameObject[] entities; // array of gameobject entities to randomly spawn
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(SpawnEnemy());
+	}
+
+    // get random position for spawning
+    private Vector3 GetRandomPosition(Renderer renderer)
+    {
+        Vector3 position = Camera.main.ScreenToWorldPoint(new Vector3(Random.Range(renderer.bounds.size.x, Screen.width - renderer.bounds.size.x), Random.Range(Screen.height + 100, Screen.height + 200), 0));
+        position.z = 0;
+        return position;
+    }
+
+    // get random entity to spawn
+    private GameObject GetRandomEntity() {
+        return entities[Random.Range(0, entities.Length - 1)];
+    }
+
+    // spawn coroutine
+    IEnumerator SpawnEnemy() {
+        while(true) {
+            GameObject entity = GetRandomEntity();
+            Vector3 position = GetRandomPosition(entity.GetComponent<Renderer>());
+            Quaternion rotation = Quaternion.Euler(0, 0, 180);
+            Instantiate(entity, position, rotation);
+            yield return new WaitForSeconds(Random.Range(spawnDelay - 0.5f, spawnDelay + 2f));
+        }
+    }
+}
