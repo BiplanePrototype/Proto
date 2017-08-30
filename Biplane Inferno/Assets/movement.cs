c83954fda6893c549d8df69b334bd170d0ec5bca
jdihle <jakx76@gmail.com>
2017-08-30 13:05:17 -0600

    clamped to screen
    yay

--- Biplane Inferno/Assets/movement.cs
+++ Biplane Inferno/Assets/movement.cs
@@ -4,24 +4,33 @@ using UnityEngine;
 
 public class movement : MonoBehaviour {
     Rigidbody2D rb;
-
+    Vector2 prevpos;
+    double width = 1.9;
     [SerializeField]
     float playerSpeed = 1f;
     // Use this for initialization
     void Start () {
          rb = GetComponent<Rigidbody2D>();
+        prevpos = new Vector2(0, 0);
     }
 
     // Update is called once per frame
     void Update () {
+        var dist = (transform.position.y - Camera.main.transform.position.y);
         if (Input.GetKey(KeyCode.A)||Input.GetKey(KeyCode.LeftArrow))//movement left
         {
-            rb.AddForce(-transform.right * Time.deltaTime * playerSpeed, ForceMode2D.Impulse);
+                rb.AddForce(-transform.right * Time.deltaTime * playerSpeed, ForceMode2D.Impulse);
         }
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))//movement right
         {
-            rb.AddForce(transform.right * Time.deltaTime * playerSpeed, ForceMode2D.Impulse);
+                rb.AddForce(transform.right * Time.deltaTime * playerSpeed, ForceMode2D.Impulse);
         }
+        if (gameObject.transform.position.x-width < Camera.main.ViewportToWorldPoint(new Vector3(0, 0, dist)).x|| gameObject.transform.position.x+width > Camera.main.ViewportToWorldPoint(new Vector3(1, 0, dist)).x)
+        {
+            rb.velocity = new Vector2(0, 0);
+            gameObject.transform.position = prevpos;
+        }
+        prevpos = gameObject.transform.position;
     }
     
 }
