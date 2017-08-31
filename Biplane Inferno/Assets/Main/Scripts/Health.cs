55ff2161a430b37723746ae3fa445cb492b191de
jdihle <jakx76@gmail.com>
2017-08-30 20:54:49 -0600

    added player and enmey health
    level restarts when player dies and enemies r destroyed.

---
+++ Biplane Inferno/Assets/Main/Scripts/Health.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Health : MonoBehaviour {
+
+    // Use this for initialization
+    public int health;
+    Rigidbody2D rb;
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+        {
+            health--;
+        }
+        if (collision.gameObject.tag == "Plane")
+        {
+            health-=5;
+        }
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Biplane Inferno/Assets/Main/Scripts/HealthEN.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthEN : MonoBehaviour {
+
+    // Use this for initialization
+    public int health;
+    Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+        {
+            health--;
+        }
+        if (collision.gameObject.tag == "Player")
+        {
+            health =0;
+        }
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
